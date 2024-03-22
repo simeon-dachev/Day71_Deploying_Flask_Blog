f3057f8a66b9200d761bb66dd1c450adf757ce9a
Simeon <simeon.dachevv@gmail.com>
2024-03-22 17:35:34 +0100

    Use env variables and create Procfile.

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+# Move to environment variables
+import os
 
 
 '''
@@ -28,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__, instance_path="C:/Users/Lenovo/Desktop/Udemy_courses/Python_100-day_Bootcamp/Day69_Blog_Flask_Authentication/instance")
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -40,7 +42,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -239,4 +241,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
