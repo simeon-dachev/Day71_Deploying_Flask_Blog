10848b81b124811f458735224f472a5d2699e99e
Simeon <simeon.dachevv@gmail.com>
2024-03-22 19:36:34 +0100

    Use env variables and create Procfile.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import date
-from alembic import op
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
